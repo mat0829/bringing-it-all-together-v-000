6554d0093e3606a5fc96d4bf16ef9d2feededeaf
David Anderson <matandersonlv@hotmail.com>
2019-05-21 22:12:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,7 +68,7 @@ class Dog
   end 
   
   def self.find_by_name(name)
-    sql = "SELECT * FROM dogs WHERE id = ?"
+    sql = "SELECT * FROM dogs WHERE name = ?"
     result = DB[:conn].execute(sql, id)[0]
     Dog.new(result[0], result[1], result[2])
   end
