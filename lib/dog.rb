2070150aafaff22f7df2c333f832260131902174
David Anderson <matandersonlv@hotmail.com>
2019-05-21 21:16:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,7 +32,7 @@ class Dog
   end
  
   def self.find_by_id(id)
-    sql = "SELECT * FROM songs WHERE id = ?"
+    sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql, id)[0]
     Song.new(result[0], result[1], result[2])
   end
