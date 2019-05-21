2655fe15f1e3e8ea11daec7d49ba13d03fec3a68
David Anderson <matandersonlv@hotmail.com>
2019-05-21 21:28:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,7 +26,17 @@ class Dog
   end
   
   def save
-    
+    if self.id 
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, album) 
+        VALUES (?, ?)
+      SQL
+ 
+      DB[:conn].execute(sql, self.name, self.album)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
   end
   
 end
